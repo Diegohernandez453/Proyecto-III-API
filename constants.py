3dd13bd908bd94612a4013eeecd0f22661093ffd
aleperezz23 <dapp231104@gmail.com>
2025-05-25 23:47:53 -0400

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,26 @@
+#tamaños
+WIDTH, HEIGHT= 800, 600
+PLAYER = 40  
+GRASS = 64
+TREE = 64
+SMALL_STONE = 32
+
+#colores
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BROWN = (139, 69, 19)
+
+#barras de estados
+MAX_ENERGY = 100
+MAX_FOOD = 100
+MAX_THIRST = 100
+
+#Colores para barras de estado
+ENERGY_COLOR = (255, 255, 0) #AMARIILO
+FOOD_COLOR = (255, 165, 0) #NARANJA
+THIRST_COLOR = (0, 191, 255) #AZUL CLARO
+BAR_BACKGROUND = (100,100,100) #GRIS OSCURO
+
+#Intervalo de tiempo
+STATUS_UPDATE_INTERVAL = 1000
